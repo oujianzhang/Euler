250f6d948ea8a062bd0a20f4245bbe787001e5c0
oujianzhang <oujian.zhang@outlook.com>
2020-03-10 13:12:05 +0100

    third_commit

--- src/Exa_404_Euler59/XOR_Decryption_Launcher.java
+++ src/Exa_404_Euler59/XOR_Decryption_Launcher.java
@@ -5,14 +5,48 @@
  */
 package Exa_404_Euler59;
 
+import java.util.Set;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author lenovo
  */
 public class XOR_Decryption_Launcher {
 
-    String[] commonWords = new String[]{"the", "of", "and", "a", "to",
-        "in", "is", "you", "that", "it"};
-    
-    
+    public static void perform_XOR_Decryption() {
+        ExecutorService pool = Executors.newFixedThreadPool(4);
+        CompletionService service = new ExecutorCompletionService(pool);
+
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 26; j++) {
+                for (int k = 0; k < 26; k++) {
+                    service.submit(new XOR_Decryption_Worker("" + ('a' + i) + "" + ('a' + j) + "" + ('a' + k)));
+                }
+            }
+        }
+        pool.shutdown();
+        
+        while(!pool.isTerminated())
+        {
+            try {
+                Future<Integer> future = service.take();
+                
+            } catch (InterruptedException ex) {
+                Logger.getLogger(XOR_Decryption_Launcher.class.getName()).log(Level.SEVERE, null, ex);
+            }   
+        }
+    }
+
+    public static void main(String[] args) {
+        perform_XOR_Decryption();
+    }
+
 }
--- src/Exa_404_Euler59/XOR_Decryption_Worker.java
+++ src/Exa_404_Euler59/XOR_Decryption_Worker.java
@@ -5,30 +5,69 @@
  */
 package Exa_404_Euler59;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Set;
 import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author lenovo
  */
-public class XOR_Decryption_Worker implements Callable<String>{
-    
-    
+public class XOR_Decryption_Worker implements Callable<Integer> {
+
     private String key;
-    
-    
-    @Override
-    public String call() throws Exception {
-        
-        for (int i = 0; i < ; i++) {
-            for (int j = 0; j < ; j++) {
-                for (int k = 0; k < ; k++) {
-                    
+    private static String[] commonWords
+            = new String[]{"the", "of", "and", "a", "to",
+                "in", "is", "you", "that", "it"};
+    private int common_words;
+    private String encrypted_text;
+    private String decrypted_text;
+    private Path path = Paths.get(System.getProperty("user.dir"), "src", "Exa_404_Euler59", "p059_cipher.txt");
+
+    public XOR_Decryption_Worker(String key) {
+        this.key = key;
+    }
+
+    public void XOR_Input() {
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(path.toFile()));
+            while ((encrypted_text += br.readLine()) != null) {
+                encrypted_text += br.readLine();
+            }
+            
+            String[] ascii_tokens = encrypted_text.split(",");
+            encrypted_text = "";
+            int cnt = 0;
+            for (String ascii_token : ascii_tokens) {
+                encrypted_text += (char) ( Integer.parseInt(ascii_token) ^ key.charAt(cnt++));
+                if(cnt%3==0)
+                {
+                    cnt = 0;
                 }
             }
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
         }
-        
-        return key;
     }
-    
+
+    @Override
+    public Integer call() throws Exception {
+        
+        for (String commonWord : commonWords) {
+            if(encrypted_text.contains(commonWord))
+            {
+                common_words++;
+            }
+        }
+        return common_words;
+    }
+
 }
